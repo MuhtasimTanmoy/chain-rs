5157e93ac24b0b846efdcac8eed1a12b2caa4b97
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2023-03-04 11:20:26 +0600

    Message sender add.

---
+++ docs/networking.md
@@ -0,0 +1,9 @@
+# Networking
+
+There will be three types of node
+- Central Node
+  - Other nodes connect to it, it sends data to others
+- Miner Node
+  - Gathers transactions in memory pool and after reaching a threshold mine a new block and broadcast to the network
+- Wallet Node
+  - Send coins between wallets, like [SPV](https://learn.saylor.org/mod/page/view.php?id=36320) Node
---
+++ src/message_sender.rs
@@ -0,0 +1,87 @@
+use bincode::serialize;
+use log::info;
+use crate::block::Block;
+use crate::message::{BlockMessage, DataRequestType, GetBlockMessage, GetDataMessage, InventoryMessage, TxMessage, VersionMessage};
+use crate::r#const::VERSION;
+use crate::node::Node;
+use crate::parser_util::cmd_to_bytes;
+use crate::transaction::Transaction;
+
+impl Node {
+
+    pub(crate) fn send_block(&self, addr: &str, b: &Block) -> Result<(), failure::Error> {
+        info!("send block data to: {} block hash: {}", addr, b.get_hash());
+        let data = BlockMessage {
+            from: self.address.clone(),
+            block: b.clone(),
+        };
+        let data = serialize(&(cmd_to_bytes("block"), data))?;
+        self.send_data(addr, &data)
+    }
+
+    pub(crate) fn send_addr(&self, addr: &str) -> Result<(), failure::Error> {
+        info!("send address info to: {}", addr);
+        let nodes = self.get_known_nodes();
+        let data = serialize(&(cmd_to_bytes("addr"), nodes))?;
+        self.send_data(addr, &data)
+    }
+
+    pub(crate) fn send_inv(&self, addr: &str, kind: &str, items: Vec<String>) -> Result<(), failure::Error> {
+        info!(
+            "send inv message to: {} kind: {} data: {:?}",
+            addr, kind, items
+        );
+        let data = InventoryMessage {
+            from: self.address.clone(),
+            kind: kind.to_string(),
+            items,
+        };
+        let data = serialize(&(cmd_to_bytes("inv"), data))?;
+        self.send_data(addr, &data)
+    }
+
+    pub(crate) fn send_get_blocks(&self, addr: &str) -> Result<(), failure::Error> {
+        info!("send get blocks message to: {}", addr);
+        let data = GetBlockMessage {
+            from: self.address.clone(),
+        };
+        let data = serialize(&(cmd_to_bytes("getblocks"), data))?;
+        self.send_data(addr, &data)
+    }
+
+    pub(crate) fn send_get_data(&self, addr: &str, kind: DataRequestType, id: &str) -> Result<(), failure::Error> {
+        info!(
+            "send get data message to: {} kind: {} id: {}",
+            addr, kind, id
+        );
+        let data = GetDataMessage {
+            from: self.address.clone(),
+            kind,
+            id: id.to_string(),
+        };
+        let data = serialize(&(cmd_to_bytes("getdata"), data))?;
+        self.send_data(addr, &data)
+    }
+
+    pub fn send_tx(&self, addr: &str, tx: &Transaction) -> Result<(), failure::Error> {
+        info!("send tx to: {} txid: {}", addr, &tx.id);
+        let data = TxMessage {
+            from: self.address.clone(),
+            transaction: tx.clone(),
+        };
+        let data = serialize(&(cmd_to_bytes("tx"), data))?;
+        self.send_data(addr, &data)
+    }
+
+    pub(crate) fn send_version(&self, addr: &str) -> Result<(), failure::Error> {
+        info!("send version info to: {}", addr);
+        let data = VersionMessage {
+            from: self.address.clone(),
+            height: self.get_best_height()?,
+            version: VERSION as i32,
+        };
+        let data = serialize(&(cmd_to_bytes("version"), data))?;
+        self.send_data(addr, &data)
+    }
+}
+
